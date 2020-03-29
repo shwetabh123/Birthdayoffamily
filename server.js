fede6f72267d7129a33f07ef1f57d55254031abe
shwetabh <shwetabh.srivastava@gmail.com>
2020-03-30 03:32:52 +0530

    make it better

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require('express')
 const app =express();
 const path=require('path');
 
-app.use(express.static(__dirname+'/dist/Birthdayforfamily'));
+app.use(express.static(__dirname+'/dist'));
 
 app.listen(process.env.PORT||8080);
 
@@ -11,7 +11,7 @@ app.listen(process.env.PORT||8080);
 app.get('/*',function(req,res)
 {
 
-res.sendFile(path.join(__dirname+ '/dist/Birthdayforfamily/index.html'));
+res.sendFile(path.join(__dirname+ '/dist/index.html'));
 
 })
 
