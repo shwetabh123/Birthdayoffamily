6669e1304240c8d253aa0322bca69f2c50e8e6e0
shwetabh <shwetabh.srivastava@gmail.com>
2020-03-30 03:29:35 +0530

    make it better

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.listen(process.env.PORT||8080);
 app.get('/*',function(req,res)
 {
 
-res.sendFile(path.join(_dirname+ '/dist/Birthdayforfamily/index.html'));
+res.sendFile(path.join(__dirname+ '/dist/Birthdayforfamily/index.html'));
 
 })
 
