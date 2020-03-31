6f76bff6d55f385afce86a4408ff4cf0e6926845
shwetabh <shwetabh.srivastava@gmail.com>
2020-03-31 20:58:22 +0530

    qa

--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -22,10 +22,14 @@ export class PersonComponent implements OnInit {
       "dob": person.dob
     }
     // this.http.post("https://api.myjson.com/bins/gdens/", this.personObj).subscribe((res:Response)=>
-    this.navItems = this.http.post("https://my-json-server.typicode.com/shwetabh123/productsnew/products/", this.personObj).subscribe(
-      (res: Response) => {
-        this.isAdded = true; console.log(res);
-      })
+    this.navItems = this.http.post("https://my-json-server.typicode.com/shwetabh123/productsnew/products/", this.personObj).subscribe
+    (
+      (res: Response) => 
+      {
+        this.isAdded = true; 
+        console.log(res);
+      }
+      )
   }
   ngOnInit() {
   }
