05af1eb0a359d4b25e7414d8138adc7ae5a9b712
shwetabh <shwetabh.srivastava@gmail.com>
2020-03-31 08:42:50 +0530

    ws

--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -26,8 +26,9 @@ this.personObj={
 }
 // this.http.post("https://api.myjson.com/bins/gdens/", this.personObj).subscribe((res:Response)=>
 
- this.http.post("https://my-json-server.typicode.com/shwetabh123/productsnew/products/", this.personObj).subscribe(
-  (res:Response)=>
+this.navItems =this.http.post("https://my-json-server.typicode.com/shwetabh123/productsnew/products/", this.personObj).subscribe(
+ 
+ (res:Response)=>
 {
 this.isAdded=true;  console.log(res);
 })}
