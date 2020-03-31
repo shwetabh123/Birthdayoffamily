47472be2f04654109b6cbaeaa87c6822b086d6ba
shwetabh <shwetabh.srivastava@gmail.com>
2020-03-31 20:49:27 +0530

    pls

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Http, Response,Headers } from '@angular/http';
+import { Http, Response, Headers } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
 
@@ -11,30 +11,35 @@ import 'rxjs/add/operator/toPromise';
 })
 export class HomeComponent implements OnInit {
   title = 'Birthday-Calender Application';
-// API_URL = 'http://localhost:5555/products';
+  // API_URL = 'http://localhost:5555/products';
   // API_URL = '../assets/products.json';
-  API_URL='https://my-json-server.typicode.com/shwetabh123/productsnew/products';
-navItems: any;
+  API_URL = 'https://my-json-server.typicode.com/shwetabh123/productsnew/products';
+  navItems: any;
   constructor(private http: Http) { }
-  id:number;
-  private headers=new Headers({'Content-Type':'application/json'    });
- products = [];
-fetchdata= function (){
-// this.http.get(`${this.API_URL}/products`).subscribe(
-//  this.http.get("https://api.myjson.com/bins/gdens").subscribe(
-this.navItems = this.http.get(`${this.API_URL}`).subscribe(
-    (res: Response)=>{
- this.products = res.json() ;
-})}
-deletePerson= function(id)
-{
-if(confirm("Are you sure?")){
- // const url =`${"https://api.myjson.com/bins/gdens"}/${id}`;
-  const url =`${"https://my-json-server.typicode.com/shwetabh123/productsnew/products"}/${id}`;
-   return  this.http.delete(url,{headers: this.headers }).toPromise()
-.then(()=>
-{
-this.fetchdata();
-})}}
-ngOnInit() {
-this.fetchdata();  }}
+  id: number;
+  private headers = new Headers({ 'Content-Type': 'application/json' });
+  products = [];
+  fetchdata = function () {
+    // this.http.get(`${this.API_URL}/products`).subscribe(
+    //  this.http.get("https://api.myjson.com/bins/gdens").subscribe(
+    this.navItems = this.http.get(`${this.API_URL}`).subscribe(
+      (res: Response) => {
+        this.products = res.json();
+      }
+    )
+  }
+  deletePerson = function (id) {
+    if (confirm("Are you sure?")) {
+      // const url =`${"https://api.myjson.com/bins/gdens"}/${id}`;
+      const url = `${"https://my-json-server.typicode.com/shwetabh123/productsnew/products"}/${id}`;
+      return this.http.delete(url, { headers: this.headers }).toPromise()
+        .then(() => {
+          this.fetchdata();
+        })
+    }
+  }
+  ngOnInit() {
+    this.fetchdata();
+  }
+
+}
--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import {Http,Response,Headers} from '@angular/http';
+import { Http, Response, Headers } from '@angular/http';
 
 @Component({
   selector: 'app-person',
@@ -8,25 +8,28 @@ import {Http,Response,Headers} from '@angular/http';
   styleUrls: ['./person.component.css']
 })
 export class PersonComponent implements OnInit {
-//  API_URL = 'http://localhost:5555/products';
-//  API_URL = './assets/products.json';
- API_URL='https://my-json-server.typicode.com/shwetabh123/productsnew/products/';
- navItems: any;
- constructor(private http:Http) { }
-confirmationString:string="New Person Details are added !!!";
-isAdded:boolean=false;
-personObj:object={};
-addNewPerson=function(person)
-{
-this.personObj={
-"name":person.name,
-"dob":person.dob
+  //  API_URL = 'http://localhost:5555/products';
+  //  API_URL = './assets/products.json';
+  API_URL = 'https://my-json-server.typicode.com/shwetabh123/productsnew/products/';
+  navItems: any;
+  constructor(private http: Http) { }
+  confirmationString: string = "New Person Details are added !!!";
+  isAdded: boolean = false;
+  personObj: object = {};
+  addNewPerson = function (person) {
+    this.personObj = {
+      "name": person.name,
+      "dob": person.dob
+    }
+    // this.http.post("https://api.myjson.com/bins/gdens/", this.personObj).subscribe((res:Response)=>
+    this.navItems = this.http.post("https://my-json-server.typicode.com/shwetabh123/productsnew/products/", this.personObj).subscribe(
+      (res: Response) => {
+        this.isAdded = true; console.log(res);
+      })
+  }
+  ngOnInit() {
+  }
 }
-// this.http.post("https://api.myjson.com/bins/gdens/", this.personObj).subscribe((res:Response)=>
-this.navItems =this.http.post("https://my-json-server.typicode.com/shwetabh123/productsnew/products/", this.personObj).subscribe(
-  (res:Response)=>
-{
-this.isAdded=true;  console.log(res);
-})}
-ngOnInit() {
-  }}
+
+
+
