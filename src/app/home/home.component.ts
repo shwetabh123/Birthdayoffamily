323e787769df9cae9b96adcdc0d7a4129ed4288c
shwetabh <shwetabh.srivastava@gmail.com>
2020-03-31 08:57:25 +0530

    pl

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -32,14 +32,15 @@ fetchdata= function (){
 })}
 
 
-deletePerson= function(id){
+deletePerson= function(id)
+{
 if(confirm("Are you sure?")){
  // const url =`${"https://api.myjson.com/bins/gdens"}/${id}`;
 
-  const url =`${"https://my-json-server.typicode.com/shwetabh123/productsnew/products"}/${this.id}`;
+  const url =`${"https://my-json-server.typicode.com/shwetabh123/productsnew/products"}/${id}`;
    
 
-return  this.http.delete(url,{headers: this.headers }).toPromise()
+return  this.http.delete(this.url,{headers: this.headers }).toPromise()
 .then(()=>
 {
 this.fetchdata();
